86ab090d109897b205042e38e5198be8756976bf
minggang.li <minggang.li@ae3d646a-d012-11de-9e88-ff79fe1e0c1e>
2014-02-15 12:57:23 +0000

    Fix compiling issue under FreeBSD

--- charset-detector/src/prmem.h
+++ charset-detector/src/prmem.h
@@ -37,7 +37,9 @@
 #ifndef nsDummyPrmem_h__
 #define nsDummyPrmem_h__
 
+#pragma GCC visibility push(default)
 #include <stdlib.h>
+#pragma GCC visibility pop
 
 inline void* PR_Malloc(size_t len)
 {
